5ec0e12cd86af33d2f1ba7f033037ab4c5bd0fd2
dasturchioka <dasturchioka@gmail.com>
2025-07-21 12:12:53 +0500

    bugs fixed

--- next-sitemap-config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  siteUrl: 'https://novdaunion.uz',
-  generateRobotsTxt: true,
-}
---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://novdaunion.uz',
+  sitemapSize: 5000,
+  changefreq: 'monthly',
+  priority: 0.7,
+}
