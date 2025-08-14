6dfbefa1f35c176156113843baf8ce9b42797558
dasturchioka <dasturchioka@gmail.com>
2025-08-14 19:53:50 +0500

    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,20 +8,7 @@ import Seo from '../components/seo'
 export default function HomePage() {
 	return (
 		<>
-			<Seo
-				title='Novda Union'
-				url='https://novdaunion.uz/'
-			/>
-			<div className='h-screen flex items-center justify-center'>
-				<div className='bg fixed inset-0 w-full h-screen'>
-					<Silk speed={5} scale={1} color='#263BB5' noiseIntensity={1.5} rotation={0} />
-				</div>
-				<div className='logo'>
-					<div className='logo-container relative flex flex-col items-center justify-center'>
-						<Image className='w-[100px]' src={NovdaLogo} alt='Novda Union' />
-					</div>
-				</div>
-			</div>
+			<div>Failed organization.</div>
 		</>
 	)
 }
